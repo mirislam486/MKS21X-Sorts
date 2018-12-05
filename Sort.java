0a1af05003ae38f13ff0efa8727f1e932d03e253
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2018-12-05 02:17:35 -0500

    Add files via upload

--- Sort.java
+++ Sort.java
@@ -1,21 +1,20 @@
-public class Sort{
+public class Sort{ //This entire thing is probably way more complicated than it should be
 
-  public static void selectionsort(int[] ary) {
-     int[] output = int[ary.length];
-     for(int i = 0; i < ary.length; i++){
-       for(int z = 0; z < ary.length; z++){
-         if()
-       }
+  public static void selectionsort(int[] ary){
+    int[] output = new int[ary.length];
+    for(int idx = 0; idx < ary.length; i++){
 
-     }
-  }
-  public static int findMin(int[] ary){
-    for(int i = 0; i < ary.length; i++){
-      for(int z = 0; z < ary.length; z++){
-        if(ary[i] < ary[z]){
-          
-        }
-      }
     }
+  }
+
+  public static int findMin(int[] ary){ //Helper function to find the minimum
+    int min = ary[0];
+    for(int i = 0; i < ary.length; i++){
+      if(ary[i] < min){
+        min = ary[i]
+      }
+      ary[i] = ary[i];
+    }
+    return min;
   }
 }
