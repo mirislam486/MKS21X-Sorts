f107bea73d29091944fc3a5a545184a341fbdaea
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2018-12-11 14:51:21 -0500

    Add files via upload

--- Sorts.java
+++ Sorts.java
@@ -24,11 +24,22 @@ public class Sorts{
         if(data[i] > data[k]){
           int temp = data[i];
           data[i] = data[k];
-          data[k] = data[i]
+          data[k] = data[i];
           swapped = true;
         }
       }
     }
     System.out.println(ary);
+  }
+
+  public static void insertionSort(){
+    for(int i = 1; i < ary.length; i++){
+      int backup = ary[i];
+      while(c < i && ){
+        if(backup > ary[c] && backup < ary[c+1]){
+
+        }
+      }
+    }
   }
 }
